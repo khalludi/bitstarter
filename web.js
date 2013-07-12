6deb76f81c1968b73e7ece351b53f760ea4972ee
Khalid Ali <khalludi123@gmail.com>
2013-07-12 22:25:53 +0000

    put double quotes and listed full path to index.html

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 // response.send('Hello World 2!');
-    fs.readFileSync(index.html)
-    var buffer = new Buffer(index.html);
+    fs.readFileSync('~/bitstarter/index.html')
+    var buffer = new Buffer('index.html');
     buffer.toString("utf-8") 
 });
 
