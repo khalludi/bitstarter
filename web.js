885cb6bbb97bba8a30414a5e74de00f12ba032da
Khalid Ali <khalludi123@gmail.com>
2013-07-12 23:32:27 +0000

    something changed

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 // response.send('Hello World 2!');
 //    fs.readFileSync(index.html)
-    var buffer = new Buffer (20);
-    response.send(buffer.toString("utf-8", fs.readFileSync('index.html'))) 
+    var buffer = new Buffer ('index.html');
+    response.send(buffer.toString("utf-8")) 
 });
 
 var port = process.env.PORT || 5000;
