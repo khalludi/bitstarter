58030df423885faea34d453ad6adcf0d9246aaef
Khalid Ali <khalludi123@gmail.com>
2013-07-12 23:27:59 +0000

    something changed

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 // response.send('Hello World 2!');
 //    fs.readFileSync(index.html)
-    var buffer = new buffer (20);
+    var buffer = new Buffer (20);
     response.send(buffer.toString("utc 8", fs.readFileSync('index.html'))) 
 });
 
